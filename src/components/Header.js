df4e9695de92364ea25927c349606c3b8765ed52
Fred OLIVE <frol.developpement@gmail.com>
2020-09-28 16:34:12 +0200

    add header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from '../logo.svg';
+import './Header.css';
 
 const Header = () => {
 	return (
-		<header className='App-header'>
-			<img src={logo} className='App-logo' alt='logo' />
+		<header className='Header-header'>
+			<img src={logo} className='Header-logo' alt='logo' />
 			<h1>Accueil</h1>
 		</header>
 	);

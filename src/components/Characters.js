8b1c3f9e93506722664f0f4db230cacb8ac63193
Fred OLIVE <frol.developpement@gmail.com>
2020-09-29 11:38:30 +0200

    create loader

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 
-import DisplayCharacter from "./DisplayCharacter"
+import DisplayCharacter from "./DisplayCharacter";
 import "./Characters.css";
 
 class Characters extends Component {
   state = {
     characters: [],
+    isLoaded: false,
   };
 
   getCharacters = () => {
     fetch("https://akabab.github.io/starwars-api/api/all.json")
       .then((res) => res.json())
-      .then((res) => this.setState({ characters: res }));
+      .then((res) => this.setState({ characters: res, isLoaded: true }));
   };
 
   componentDidMount() {
@@ -19,15 +20,22 @@ class Characters extends Component {
   }
 
   render() {
+    const { characters, isLoaded } = this.state
     return (
-      <div>
-        <h1>Characters :</h1>
-        <div>
-          {this.state.characters.map((character) => (
-            <DisplayCharacter character={character} key={character.id} />
-          ))}
-        </div>
-      </div>
+      <>
+        {!isLoaded ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            <h1>Characters :</h1>
+            <div>
+              { characters.map((character) => (
+                <DisplayCharacter character={character} key={character.id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </>
     );
   }
 }
